93e1ecf61b6a76fd7e4e1c409e42646475cf5f7e
Andrew Garratt <Andrew.GarrattN@outlook.com>
2024-02-11 21:49:34 -0800

    edited checkwinner function to allow for both players to register as winners

--- tictactoe.js
+++ tictactoe.js
@@ -7,91 +7,116 @@ const playerFactory = (name, marker) => {
 }
 
 
-// console.log(activePlayer)
-
-
 
 const Game = (() => {
     let gameboard = ['','','','','','','','','',]
     const board = document.querySelector('.gameboard')
-    let gameIsOver = false
+    let gameOver = false
     const playerTurn = document.querySelector('.player-turn')
 
-    const player1 = playerFactory('player1', 'cross')
-    const player2 = playerFactory('player2', 'circle')
-    // let activePlayer
+    const player1 = playerFactory('Player1', 'cross')
+    const player2 = playerFactory('Player2', 'circle')
+    
     let activePlayer = player1
-    // const gameStop = () => {
-    //     if (gameIsOver) {
-    //         square.removeEventListener('click')
-    //     }
-    // }
-
-    // const gameOver = () => {
-    //     if 
-    // }
+    // let activePlayer
+    
   
+    let squares
+    let winnerDeclared = false
 
     const restartBtn = document.querySelector('.restart')
+
     restartBtn.addEventListener('click', ()=> {
-        // mark.classList.remove(activePlayer.marker)
-        gameboard = ['','','','','','','','','',]
-        console.log(gameboard)
-        console.log('click')
+        let gameboard = ['','','','','','','','','',]
+        board = ''
+        console.log(board)
         render()
+
+        // for (let i = 0; i < gameboard.length; i ++) {
+        //     gameboard.update(i, '')
+        // }
+        // gameOver = true
+        // board = ''
+        // render()
+        // marks.forEach((mark) => {
+        //     mark.classList.remove(activePlayer.marker)
+        //     // marks = ''
+        //     // for (let i = 0; i <= marks.length; i++) {
+        //         // mark.remove()
+        //         gameboard = ['','','','','','','','','',]
+        //         console.log(gameboard)
+        //         console.log('click')
+        //         // board = ''
+
+        //         ren der()
+
+        //     // }
+       
+        // }
+        // )
     })
 
-    // GameController.test()
-  
     
+        
+
+  
+    let marks 
    
     const render = () => {
 
-        for (let i = 0; i < gameboard.length; i++) {
-            const square = document.createElement('div')
-            square.classList.add('square')
-            square.id = i
-            square.addEventListener("click", () => {
+        
+        
+            for (let i = 0; i < gameboard.length; i++) {
+                const square = document.createElement('div')
+                square.classList.add('square')
+                // let squares = querySelectorAll('.square')
                 
-                const mark = document.createElement('div')
-                mark.classList.add(activePlayer.marker)
-                square.append(mark)
-                
-                square.style.pointerEvents = 'none'
-                gameboard[i] = activePlayer.marker
-                activePlayer = activePlayer === player1 ? player2 : player1
-                // DisplayController.displaySwitch()
+                // console.log(squares)
+                square.id = i
                 playerTurn.innerText = `${activePlayer.name}'s Turn!`
-                GameController.checkForTie()
-                GameController.checkWinner()
-                console.log(gameboard)
-                console.log(activePlayer.name)
+                square.addEventListener("click", () => {
+                    
+                   
+                    
+                    const mark = document.createElement('div')
+                    // marks += mark
+                    mark.classList.add(activePlayer.marker)
+                    square.append(mark)
+                    
+                    square.style.pointerEvents = 'none'
+                    gameboard[i] = activePlayer.marker
+                    activePlayer = activePlayer === player1 ? player2 : player1
+                    // DisplayController.displaySwitch()
+                    playerTurn.innerText = `${activePlayer.name}'s Turn!`
+                    GameController.checkForTie()
+                    GameController.checkWinner()
+                    gameEnd()
+                    // console.log(activePlayer.marker)
+                    // console.log(gameboard)
+                    // console.log(activePlayer.name)
+                    // console.log(squares)
+                    // console.log(marks)
+                    console.log(board)
+                    
+                    
+                })
+                squares += square
+                board.append(square)
                 
                 
-                
-            })
-            
-            board.append(square)
-            
-        }
+            }   
+       return {
+        activePlayer
+       }
+    }
+    // console.log(board.childNodes())
 
-            // const squares = document.querySelectorAll('.square')
-            // squares.forEach((square) => 
-            
-            
-        
-           
-                
-            // console.log(square)
-                
-            
-            // square.addEventListener('click', GameController.doStuff)
-        
-        
+    const gameEnd = () => {
+        if (Game.winnerDeclared === true) {
+            console.log('game Over')
+        }
     }
 
-   
-    
     return {
         render,
         gameboard,
@@ -100,42 +125,58 @@ const Game = (() => {
         playerTurn,
         player1,
         player2,
+        gameEnd
     }
 
 })();
 
+// board.getchildnodes = squares
 
 
 const GameController = (() => {
 
    Game.render()
 
-    const checkWinner = () => {
-        const winCombos = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ];
+   const checkWinner = () => {
+    const winCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    // Game.activePlayer = Game.activePlayer === Game.player1 ? Game.player2 : Game.player1
+
+    // I think the reason only one player can win is because this function perpetually
+    // recognizes activeplayer as the player that is declared active player above
     
-      winCombos.forEach((item) => {
-        for (let i = 0; i <= item.length; i++) {
-            if (Game.gameboard[item[0]] === Game.activePlayer.marker && 
-                Game.gameboard[item[1]] === Game.activePlayer.marker && 
-                Game.gameboard[item[2]] === Game.activePlayer.marker)
-        {
-            // gameIsOver = true
-            winnerDeclared = true
-            Game.playerTurn.innerText = `${Game.activePlayer.name} wins!`
-            console.log(`${Game.activePlayer.name} wins!`)
-        }
-        }
-        
-    })}
+
+  winCombos.forEach((item) => {
+    for (let i = 0; i <= item.length; i++) {
+        if (Game.gameboard[item[0]] === 'cross' && 
+            Game.gameboard[item[1]] === 'cross' && 
+            Game.gameboard[item[2]] === 'cross')
+    {
+        // gameIsOver = true
+        Game.winnerDeclared = true
+        Game.playerTurn.innerText = `Player1 wins!`
+        console.log(`Player1 wins!`)
+
+    } else if (Game.gameboard[item[0]] === 'circle' && 
+            Game.gameboard[item[1]] === 'circle' && 
+            Game.gameboard[item[2]] === 'circle') {
+
+        Game.winnerDeclared = true
+        Game.playerTurn.innerText = `Player2 wins!`
+        console.log(`Player2 wins!`)
+    }
+    }
+    
+})}
 
 
     const checkForTie = () => {
@@ -143,7 +184,8 @@ const GameController = (() => {
                 if (Game.gameboard[0] !== '' && Game.gameboard[1] !== '' && Game.gameboard[2] !== '' && 
                     Game.gameboard[3] !== '' && Game.gameboard[4] !== '' && Game.gameboard[5] !== '' && 
                     Game.gameboard[6] !== '' && Game.gameboard[7] !== '' && Game.gameboard[8] !== '') {
-                    Game.playerTurn.innerText = 'Tie Game'
+                    
+                        Game.playerTurn.innerText = 'Tie Game'
 
                 }
             }     
@@ -163,47 +205,3 @@ const GameController = (() => {
 
 })();
 
-
-
-
-
-
-// this will deal with the UI, what text is popping up, etc
-const DisplayController = (() => {
-    // const playerTurn = document.querySelector('.player-turn') 
-
-    // Game.activePlayer = Game.activePlayer === Game.player1 ? Game.player2 : Game.player1
-    const turn = () => {
-
-    }
-    Game.playerTurn.innerText = `${Game.activePlayer.name}'s Turn!`
-
-    const displaySwitch = () => {
-        
-        Game.playerTurn.innerText = `${Game.activePlayer.name}'s Turn!`
-    }
-
-    const changePlayer = () => {
-        Game.playerTurn.innerText = Game.activePlayer.name
-    }
-   
-     return {
-        // playerTurn,
-        displaySwitch,
-        changePlayer,
-
-    }
-
-})();
-
-// DisplayController.playerTurn.innerText = 'hello'
-
-
-//   const declareWinner = () => {
-
-//     if (gameIsOver === true) {
-//         playerTurn.innerText = `${activePlayer.name} wins!`
-//         console.log(`${activePlayer.name} wins!`)
-//     }
-
-//   }
